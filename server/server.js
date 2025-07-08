68890c4c3483d3d50569cb48fa77763958e16d4b
sumu9897 <sumon.bncd@gmail.com>
2025-07-08 15:30:20 +0600

    backend Setup

---
+++ server/server.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import cors from 'cors';
+import "dotenv/config";
+
+
+const app = express()
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res)=> res.send("Server is Running"))
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, ()=> console.log(`Running on port ${PORT}`))
