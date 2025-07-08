388b6ca1025669222eaabdfd3e0aa84d0f23cfdf
sumu9897 <sumon.bncd@gmail.com>
2025-07-08 19:45:51 +0600

    User data Show Successfully

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -52,5 +52,13 @@ export const loginUser = async (req, res) => {
   }
 };
 
-
-
+// Get User data useing Token (JWT)
+export const getUserData = async (req, res) => {
+  try {
+    const { user } = req;
+    res.json({ success: true, user });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
