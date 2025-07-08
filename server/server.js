b3369cfd65d087e8e901ba3a991f8ef1038c0d21
sumu9897 <sumon.bncd@gmail.com>
2025-07-08 19:12:09 +0600

    User Login registration part done

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors';
 import "dotenv/config";
 import connectDB from './configs/db.js';
+import userRouter from './routes/userRoutes.js';
 
 
 const app = express()
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res)=> res.send("Server is Running"))
+app.use('/api/user', userRouter)
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=> console.log(`Running on port ${PORT}`))
